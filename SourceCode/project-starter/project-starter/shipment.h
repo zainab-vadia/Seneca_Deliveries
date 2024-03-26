926845866e72977b66a43b6e95bdf9e6b53dfe17
Chieh Sung <csung7@myseneca.ca>
2024-03-26 13:11:29 -0400

    Update shipment.h

--- SourceCode/project-starter/project-starter/shipment.h
+++ SourceCode/project-starter/project-starter/shipment.h
@@ -5,7 +5,7 @@
 #include "mapping.h"
 #include "truck.h"
 
-#define VALID_BOX_VOLUME {0.5, 1, 5} 
+#define VALID_BOX_VOLUME {0.5, 1, 5}
 
 /**
 * A package represents a delivery item with weight, volume, and destination.
@@ -18,17 +18,18 @@ struct Shipment
 };
 
 /**
-* input a valid shipment from the terminal
+* Read shipment details from the terminal
 * @returns - an initialized package if valid, otherwise an empty shipment
 */
-struct Shipment inputShipment();
+struct Shipment readShipmentDetails();
 
 /**
-* validate the package and provide an error message 
+* Validates the shipment details and provides an error message if any
 * @param weight - the weight of the package
 * @param volume - the volume of the package
 * @param destination - the destination of the package
 * @returns - true if the shipment is valid, false otherwise
 */
-int isValidPackage(double weight, double volume, struct Point destination);
+int validateShipment(double weight, double volume, struct Point destination);
+
 #endif
