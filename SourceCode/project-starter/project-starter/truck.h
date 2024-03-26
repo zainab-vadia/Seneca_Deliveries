c1407ff5dfa5a47097f95d90413588b4963f7c77
Chieh Sung <csung7@myseneca.ca>
2024-03-26 19:37:52 -0400

    Update truck.h

--- SourceCode/project-starter/project-starter/truck.h
+++ SourceCode/project-starter/project-starter/truck.h
@@ -22,9 +22,8 @@ struct Truck
     // the location must lie on the route
 };
 
-
 /**
-* Initialize a truck with the specified weight, volume, and route.
+* Initialize a truck with the  weight, volume, and route.
 * @param route - the route of the truck
 * @returns - a configurized truck
 */
@@ -32,35 +31,35 @@ struct Truck configureTruck(struct Route route, char color);
 
 /**
 * Find the appropriate truck to deliver a package based on available space ,volume and distance.
-* @param trucks - array of trucks available for delivery
-* @param numTrucks - the number of trucks in the array
-* @param shipment - the package to be delivered
-* @param map - the map to be referenced
-* @param color - the color or identifier for the truck's route
-* @returns - the index of the best truck to deliver the package, or -1 if no suitable truck is found
+* @param trucks - array of delivery trucks available
+* @param numOfTrucks - the number of trucks that are in array
+* @param shipment - the shipment to be delivered
+* @param map - the map to refer to
+* @param color - the color of the truck's route
+* @returns - the index of the best truck to deliver the  shipment, or -1 if no suitable truck is found
 */
-void appropriateTruck(struct Truck trucks[], int numTrucks, struct Shipment shipment, const struct Map* map);
+void appropriateTruck(struct Truck trucks[], int numOfTrucks, struct Shipment shipment, const struct Map* map);
 
 /**
 * checks whether the truck has enough space for the Shipment
-* @param truck - truck that is being checked
-* @param shipment - the package to be delivered
-* @returns - true if the package can be added to a truck, false otherwise
+* @param truck - truck that is being checked for space
+* @param shipment - the shipment to be delivered
+* @returns - 1 ithe truck has enough space for the shipment, false otherwise.
 */
 int enoughSpace(struct Truck truck, struct Shipment shipment);
 
 /**
-* checks which truck has more space
-* @param truck1 - truck to be comapred
-* @param truck2 - truck to be compared
-* @returns - true if truck 1 has more space false otherwise
+*  checks for the truck with more space
+* @param t1 - truck to be comapred
+* @param t2 - truck to be compared
+* @returns - 1 if t1 has more space false otherwise
 */
-int moreSpace(struct Truck truck1, struct Truck truck2);
+int moreSpace(struct Truck t1, struct Truck t2);
 
 /**
-* Calculates the percentage of space (both volume and weight) still available in the truck.
+* Calculates the percentage space of both volume and weight left in the truck.
 * @param truck - truck to be evaluated
-* @returns - the percentage value of the space
+* @returns - the value of available percentage space left
 */
 double availablePercentageLeft(struct Truck truck);
 
@@ -72,9 +71,12 @@ void truckDiversions(struct Route* route);
 
 /**
 * print the diversion seprated by commas
-* @param truck - truck, to which the shipment should be added
+* @param truck - truck shipment is added to
 * @param shipment - shipment to be added
 */
-void loadShipmentDiversions(struct Truck* truck, const struct Shipment shipment);
+void loadDiversions(struct Truck* truck, const struct Shipment shipment);
 
 #endif
+
+
+//WRONG
